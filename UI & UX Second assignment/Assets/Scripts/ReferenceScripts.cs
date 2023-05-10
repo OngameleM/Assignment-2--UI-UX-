2733210a2ef59223f5823d167e87990adabaf697
Ongamele Mlawuli <133018808+OngameleM@users.noreply.github.com>
2023-05-10 23:01:56 +0200

    Main Menu Scene
    Just created three different buttons for the UI

---
+++ UI & UX Second assignment/Assets/Scripts/ReferenceScripts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReferenceScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(" Backgound image reference [ Pixelnest Studio (2013) Menus - loading and restarting the game, Pixelnest Studio. Available at: https://pixelnest.io/tutorials/2d-game-unity/menus/ (Accessed: 10 May 2023). ]");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
